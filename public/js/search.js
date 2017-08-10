2d8166d8ada00a35805c4d527941254c4d817033
Ferlargut <ferlargut@gmail.com>
2017-08-10 17:40:24 -0500

    conexion de paginas

--- public/js/search.js
+++ public/js/search.js
@@ -1,8 +1,9 @@
 var cargarPagina = function () {
   btnRegistroGo.click(ingresoGoogle);
-  $(document).on("click",".boton-reserva",function () {
+  $(document).on("click",".boton-reserva",function () {     
       var key = $(".key-reserva").attr("data-key");
-      console.log(key);
+      location.href = "../views/reservation.html";
+      console.log(EventTarget);
     //   localStorage.setItem("lastname", "Smith");
   });
 };
@@ -29,7 +30,7 @@ var cargarPagina = function () {
     var token = result.credential.accessToken;
     var user = result.user;
     console.log(user);
-    location.href = "/public/views/home.html";  
+    location.href = "../views/home.html";  
 
     }).catch(function(error) {
     var errorCode = error.code;
@@ -64,6 +65,7 @@ var db = firebase.database().ref().child('tours');
     var tours = snap.val();
     var plantillaFinal = "";
     var plantillaFinal2="";
+    var plantillaFinal3="";
     console.log(tours);
 
     $.each(tours, function (key, obj,e) {
@@ -73,15 +75,20 @@ var db = firebase.database().ref().child('tours');
         console.log(key);
       var locBusqueda = $("#buscar-tour");
       var articleTours = $("#tour-mostrar");
+      var reservTour=$("#reserva1");
+
       var recomendacionesTour = $("#recomendaciones-tour");
       plantillaFinal += plantillaTour.replace("__nombre__",tour.nombre).replace("__fecha__",tour.fecha)
       .replace("__descripcion__",tour.descripcion).replace("__imagen__",tour.imagen).replace("__key__",key);
+
       plantillaFinal2 += plantillaBusqueda.replace("__nombre__",tour.nombre)
       .replace("__descripcion__",tour.descripcion).replace("__imagen__",tour.imagen);
-
+      plantillaFinal3=plantillaBusqueda.replace("__nombre__",tour.nombre)
+      .replace("__descripcion__",tour.descripcion).replace("__imagen__",tour.imagen);
       locBusqueda.html(plantillaFinal2);
       articleTours.html(plantillaFinal);
       recomendacionesTour.html(plantillaFinal2);
+      reservTour.html(plantillaFinal3)
       }
 
   });
@@ -119,7 +126,24 @@ var plantillaBusqueda =
                       '</div>'+
                   '</div>'+
               '</div>';
-
+var plantillaReserva =
+    '<section class="uk-container uk-margin-large" id="detailEvent">'+
+        '<h1 class="text-gray">__nombre__</h1>'+
+        '<div class="uk-grid-divider uk-child-width-expand@s" uk-grid>'+
+            '<div class="uk-align-center">'+
+            '<img src="__imagen__" alt="">'+
+            '</div>'+
+            '<div>'+
+            '<div class="uk-child-width-expand@s uk-margin-medium-top" uk-grid>'+
+                '<p><b>FECHA:</b> 04/MAY/2017</p>'+
+                '<p><b>COSTO:</b> $500.00</p>'+
+            '</div>'+
+            '<p><b>UBICACIÓN:Bellas Artes</p>'+
+            '<p>DESCRIPCIÓN:__descripcion__</p>'+
+            '<button class="uk-button color-green text-white uk-margin-large uk-align-right" type="button" uk-toggle="target: #modal-reservation"  >Reservar</button>'+
+           ' </div>'+
+        '</div>'+
+        '</section>';
 
 
       $(document).ready(cargarPagina);
